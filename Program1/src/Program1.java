549006a558cb51d06be62165ac885004b232c8e2
ecrawson2019 <ecrawson2019@cchsraider.net>
2017-09-21 13:41:08 -0400

    initial commit

---
+++ Program1/src/Program1.java
@@ -0,0 +1,15 @@
+/* Emma Crawson
+ * Program 1
+ * 09/20/2017
+ * displays message
+ */
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("//////////////////////"); //prints message
+		System.out.println("|| Victory is mine! ||"); //prints message
+		System.out.println("//////////////////////"); //prints message
+	}
+
+}
