d0d02e4a65dcac28584e8639f659e02161f01494
ecrawson2019 <ecrawson2019@cchsraider.net>
2017-09-21 14:04:25 -0400

    first draft

--- Program2/src/Program2.java
+++ Program2/src/Program2.java
@@ -1,9 +1,20 @@
-
+/*Emma Crawson
+ * Program 2
+ * 09/21/2017
+ * displays text
+ */
 public class Program2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("A well-formed Java program has");
+		System.out.println("a main method with {and}");
+		System.out.println("braces.");
+		System.out.println("");
+		System.out.println("A System.out.println statement");
+		System.out.println("has ( and ) and usually a");
+		System.out.println("String enclosed between \" and \"");
+		
 	}
 
 }
