3a64f6e94170f71bbb3b4b64a2c715ec695b3417
emmacrawson <emmacrawson@10.0.1.10>
2017-09-24 22:17:21 -0400

    initial commit

---
+++ Program3/src/Program3.java
@@ -0,0 +1,25 @@
+/*Emma Crawson
+ * AP Computer Science A
+ * 09/25/2017
+ * displays message using methods
+ */
+public class Program3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			message1 (); //displays text found in the first method
+			message1 ();
+			message1 ();
+			message1 ();
+			message1 ();
+			message2 (); //displays text found in the second method
+	}
+		public static void message1 () {
+		System.out.println("//////////////////////"); //displays 22 /
+		System.out.println("|| Vicotry is mine! ||"); //displays text
+	}
+		public static void message2 () {
+		System.out.println("//////////////////////"); //displays 22 /
+		}
+
+}
