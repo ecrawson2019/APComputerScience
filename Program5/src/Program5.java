30b525e93e00b86bd4f93aaaee44ffb3a6c51c92
emmacrawson <emmacrawson@10.0.1.10>
2017-09-24 22:16:19 -0400

    initial commit

---
+++ Program5/src/Program5.java
@@ -0,0 +1,40 @@
+/*Emma Crawson
+ * AP Computer Science A
+ * 09/25/2017
+ * displays series of *
+ */
+public class Program5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		stars1 (); //displays first method
+		stars2 (); //displays second method
+		stars3 (); //displays thrid method
+	}
+	public static void stars1 () {
+		System.out.println("*****"); //displays stars
+		System.out.println("*****");
+		System.out.println(" * * ");
+		System.out.println("  *  ");
+		System.out.println(" * * ");
+	}
+	public static void stars2 () {
+		System.out.println("\n\n*****"); //starts two new lines and displays stars
+		System.out.println("*****");
+		System.out.println(" * * ");
+		System.out.println("  *  ");
+		System.out.println(" * * ");
+		System.out.println("*****");
+		System.out.println("*****");
+	}
+	public static void stars3 () {
+		System.out.println("\n\n  *  "); //starts two new lines and displays stars
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println(" * * ");
+		System.out.println("  *  ");
+		System.out.println(" * * ");
+	}
+}
