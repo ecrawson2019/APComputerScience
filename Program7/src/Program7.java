33b14fa784fe6235b93e57a685df4cdec019d611
emmacrawson <emmacrawson@10.0.1.10>
2017-09-24 22:14:08 -0400

    initial commit

---
+++ Program7/src/Program7.java
@@ -0,0 +1,18 @@
+/* Emma Crawson
+ * AP Computer Science A
+ * 09/25/2017
+ * displays message
+ */
+public class Program7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A \"quoted\" String is"); //displays line in message
+		System.out.println("\t'much' better if you learn");
+		System.out.println("the rules of \"esacpe squences.\"");
+		System.out.println("\tAlso, \"\" respresents and empty String.");
+		System.out.println("Don't forget: use \\\" instead of \"!");
+		System.out.println("\t'' is not the same as \"");
+	}
+
+}
